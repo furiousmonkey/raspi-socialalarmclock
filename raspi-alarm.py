fe7a52e2b72a5b7b22f2e974e2671314218e7f60
furiousmonkey <furiousmonkey@maildrop.cc>
2014-10-05 00:24:52 +1000

    tested on RasPi; changes...

--- raspi-alarm.py
+++ raspi-alarm.py
@@ -9,36 +9,47 @@ import time
 import os
 from datetime import datetime, timedelta
 
-def beginAlarm():
-	alarm_time = addSecs(datetime.now().isoformat(), 15) # for testing.
-	#alarm_sound = '/var/mp3/alarm/mp3'
+global alarm_time
+
+#alarm_sound = '/var/mp3/alarm/mp3'
 
 def checkTime():
-  check_now = datetime.now().isoformat()
-  if check_now == alarm_time:
-    print startAlarm()
-  return check_now
-    
-def startAlarm():
-  # used to start the audio, take the PIN, and either snooze or stop the alarm.
-  #alarm_sound.play()
-  date_code = datetime.now().strftime('%d%m%y')
-  stop_code = raw_input('Today\'s Date')
-  if stop_code == date_code:
-    #alarm_sound.stop()
-    return 'Good morning!'
-  else:
-    #alarm_sound.snooze()
-	alarm_time = addSecs(alarm_time, 600) 
-	return 'You lazy son-of-a...'
-  
+	while True:
+		time.sleep(1)
+		check_now = datetime.now().strftime("%H:%M:%S")
+		if check_now == alarm_time.strftime("%H:%M:%S"):
+			if checkCode():
+				print "Good morning!"
+				break
+			else:
+				print 'You lazy son-of-a...'
+		else:
+			print check_now
+
+def checkCode():
+	# used to start the audio, take the PIN, and either snooze or stop the alarm.
+	#alarm_sound.play()
+	global alarm_time
+	date_code = datetime.now().strftime('%d%m%y')
+	stop_code = raw_input('Today\'s Date: ')
+	if stop_code == date_code:
+		#alarm_sound.stop()
+		return True
+	else:
+		#alarm_sound.snooze()
+		alarm_time = addSecs(alarm_time, 10)
+		return False
+		
+
 def addSecs(tm, secs):
     fulldate = datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
     fulldate = fulldate + timedelta(seconds=secs)
     return fulldate.time()
 
-beginAlarm()
-	
+alarm_time = addSecs(datetime.now(), 10) # for testing.
+
+checkTime()
+
 '''
 Input: USB Number Pad
 Output: 1 x Speaker
