8201be277cd7a6ee2d2c3b60a69ecf5b7a468830
furiousmonkey <leeryan@westnet.com.au>
2014-10-04 12:10:52 +1000

    Update raspi-alarm.py

--- raspi-alarm.py
+++ raspi-alarm.py
@@ -13,17 +13,11 @@ from datetime import datetime, timedelta
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-
-global alarm_hour, alarm_minute, alarm_mp3
-parser = SafeConfigParser()
-parser.read('rpi_alarm.cfg')
-
-alarm_hour = parser.get('options', 'hour')
-alarm_minute = parser.get('options', 'minute')
-alarm_mp3 = parser.get('options', 'mp3')
+alarm_time = '12:10:30+01:00'
+alarm_mp3 = '/var/mp3/alarm/mp3'
 
 def checkTime():
-  check_now = "$s:$s" % (datetime.now().hour, datetime.now().minute)
+  check_now = datetime.now().isoformat()
   if check_now == alarm_time:
     print startAlarm()
   return check_now
@@ -31,15 +25,15 @@ def checkTime():
 def startAlarm():
   # used to start the audio, take the PIN, and either snooze or stop the alarm.
   alarm_sound.play()
-  date_code = datetime.now().strftime("%d%m%y")
-  stop_code = raw_input("Today\'s Date")
+  date_code = datetime.now().strftime('%d%m%y')
+  stop_code = raw_input('Today\'s Date')
   if stop_code == date_code:
     alarm_sound.stop()
-    return "Good morning!"
+    return 'Good morning!'
   else:
     alarm_sound.snooze()
     alarm_time = addSecs(alarm_time, 600) 
-  return "You lazy son-of-a..."
+  return 'You lazy son-of-a...'
   
   def addSecs(tm, secs):
     fulldate = datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
@@ -47,7 +41,7 @@ def startAlarm():
     return fulldate.time()
 	
 	
-"""
+'''
 Input: USB Number Pad
 Output: 1 x LED, 1 x Speaker
-"""
+'''
